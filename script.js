420cae1f9ec6787850ae870c68635375cd6837f7
BuildTools <unconfigured@null.spigotmc.org>
2020-03-27 23:37:37 +0100

    Added Covid data request

--- script.js
+++ script.js
@@ -4,7 +4,20 @@ document.addEventListener('DOMContentLoaded', (event) => {
 
 
 document.initialize = function() {
-    teamspeakButton = this.getElementsByClassName("teamspeak_button")[0];
-    console.log(teamspeakButton);
-    console.log(teamspeakButton.innerHtml);
+    // teamspeakButton = this.getElementsByClassName("teamspeak_button")[0];
+    // console.log(teamspeakButton);
+    // console.log(teamspeakButton.innerHtml);
+
+// Create a request variable and assign a new XMLHttpRequest object to it.
+    var request = new XMLHttpRequest()
+
+    // Open a new connection, using the GET request on the URL endpoint
+    request.open('GET', 'https://bing.com/covid/data', true)
+
+    request.onload = function() {
+      console.log(this.response)
+    }
+
+    // Send request
+    request.send()
 }
